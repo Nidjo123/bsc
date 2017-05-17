6392ae364f0c6c0539e1eb1dd777fcab1ce3270a
Nikola Bunjevac <nikola.bunjevac@gmail.com>
2017-05-17 17:38:21 +0200

    Changed BT cost calculation a bit

--- code/BirchfieldTomasi.cpp
+++ code/BirchfieldTomasi.cpp
@@ -3,26 +3,24 @@
 
 #define MAX_COST 99999999
 
-int BirchfieldTomasi::Cbt(int x, int y, int d, image &left_image, image &right_image) {
+int BirchfieldTomasi::Cbt(int leftX, int rightX, int y, image &left_image, image &right_image) {
   unsigned leftVal, midVal, rightVal, leftMidVal;
-  const int xp = x - d;
-  const int yp = y;
 
-  if (xp < 0) return MAX_COST;
+  if (rightX < 0) return MAX_COST;
 
-  midVal = right_image[yp][xp];
-  leftMidVal = left_image[y][x];
+  midVal = right_image[y][rightX];
+  leftMidVal = left_image[y][leftX];
   
-  if (xp > 0) {
+  if (rightX > 0) {
     // left position exists
-    leftVal = right_image[yp][xp - 1];
+    leftVal = right_image[y][rightX - 1];
   } else {
     leftVal = midVal;
   }
 
-  if (xp < width_ - 1) {
+  if (rightX < width_ - 1) {
     // right position exists
-    rightVal = right_image[yp][xp + 1];
+    rightVal = right_image[y][rightX + 1];
   } else {
     rightVal = midVal;
   }
@@ -31,13 +29,13 @@ int BirchfieldTomasi::Cbt(int x, int y, int d, image &left_image, image &right_i
   const double Ip = (midVal + rightVal) / 2.0;
   const double Im = (midVal + leftVal) / 2.0;
 
-  const double Imin = std::min(std::min(Im, Ip), (double) right_image[yp][xp]);
-  const double Imax = std::max(std::max(Im, Ip), (double) right_image[yp][xp]);
+  const double Imin = std::min(std::min(Im, Ip), (double) midVal);
+  const double Imax = std::max(std::max(Im, Ip), (double) midVal);
 
   return std::max(0.0, std::max(leftMidVal - Imax, Imin - leftMidVal));
 }
 
 int BirchfieldTomasi::calculate(int x, int y, int d) {
-  return std::min(Cbt(x, y, d, right_image_, left_image_),
-		  Cbt(x, y, d, left_image_, right_image_));
+  return std::min(Cbt(x, x - d, y, left_image_, right_image_),
+		  Cbt(x - d, x, y, right_image_, left_image_));
 }
